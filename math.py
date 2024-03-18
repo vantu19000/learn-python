29c486b50e937b86f6b2232f77d62f0b1ef5eeda
Van Tu Nguyen <vantu19000@gmail.com>
2024-03-18 14:44:27 +0700

    python

---
+++ math.py
@@ -0,0 +1,8 @@
+def get_sumary():
+    return 9+9
+
+def get_sub():
+    return 10 - 1
+
+def get_div():
+    return 100/10
---
+++ modules/user.py
@@ -0,0 +1,5 @@
+def get_name():
+    return "Nguyen Van Tu"
+
+def get_salary():
+    return 10000000
