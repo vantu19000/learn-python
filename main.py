d05dbfcf6d9e01b55d86b68e66724eee287f8419
Van Tu Nguyen <vantu19000@gmail.com>
2024-03-18 15:21:02 +0700

    bai 11

---
+++ main.py
@@ -0,0 +1,16 @@
+import tinh_toan
+import modules.user
+
+fullname = modules.user.get_name()
+print(fullname) #output: Nguyen Van Tu
+
+salary = modules.user.get_salary()
+print(salary) #output: 10000000
+
+
+
+tong = tinh_toan.get_sumary()
+print(tong) #output: 18
+
+hieu = tinh_toan.get_sub()
+print(hieu) #output 8
